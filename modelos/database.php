caa194573fd69bb82d381dd0323b881e42333e56
chachoman1 <rojasromero092@gmail.com>
2024-03-22 00:21:29 -0500

    agregamos ssl y database

--- modelos/database.php
+++ modelos/database.php
@@ -1,16 +1,18 @@
 <?php
-Class DataBase{
-    public static function conn(){
-        $hostname = "127.0.0.1";
+class DataBase
+{
+    public static function connection()
+    {
+        $hostname = "davinci-desings.mysql.database.azure.com";
         $port = "3306";
         $database = "telas";
-        $username = "root";
-        $password = "";
-
-        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+        $username = "abc";
+        $password = "Chachoman1992.";
+        $options = array(
+            PDO::MYSQL_ATTR_SSL_CA => 'assets/coneccion.pem'
+        );
+        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password, $options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
-
+    }
 }
-}
-?>
